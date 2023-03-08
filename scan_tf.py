0642a95a60784b321d974584b64f0e0ff247c5aa
xeonproc <xeonproc@gmail.com>
2023-03-08 11:35:45 -0600

    Update scan_tf.py

--- scan_tf.py
+++ scan_tf.py
@@ -4,27 +4,30 @@ import os
 # Fetch the API key from the environment variable
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-# Identify the location of the Terraform files in the repository
-terraform_dir = "/"
+# Use the OpenAI API as before
+def generate_text(prompt, files):
+    completions = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=60,
+        n=1,
+        stop=None,
+        temperature=0.5,
+        files=files
+    )
+    message = completions.choices[0].text.strip()
+    return message
 
-# Use the OpenAI API to scan each Terraform file for AWS best practices
-def scan_terraform_file(file_path):
-    with open(file_path, "r") as file:
-        prompt = file.read()
-        completions = openai.Completion.create(
-            engine="davinci",
-            prompt=prompt,
-            max_tokens=60,
-            n=1,
-            stop=None,
-            temperature=0.5,
-        )
-        message = completions.choices[0].text.strip()
-        # Add logic here to parse the message and determine if the Terraform file
-        # follows AWS foundational best practices.
+# Recursively search for all .tf files in the file system
+terraform_files = []
+for root, dirs, files in os.walk("/"):
+    for filename in files:
+        if filename.endswith(".tf"):
+            file_path = os.path.join(root, filename)
+            with open(file_path, "r") as f:
+                terraform_files.append(f.read())
 
-# Scan each Terraform file in the directory for AWS best practices
-for filename in os.listdir(terraform_dir):
-    if filename.endswith(".tf"):
-        file_path = os.path.join(terraform_dir, filename)
-        scan_terraform_file(file_path)
+# Prompt OpenAI to scan the Terraform files for AWS foundational best practices
+prompt = "Please scan the Terraform files for AWS foundational best practices."
+message = generate_text(prompt, files=terraform_files)
+print(message)
