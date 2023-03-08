969e18786a6284386916451359aadfb693f38983
xeonproc <xeonproc@gmail.com>
2023-03-08 10:51:25 -0600

    Update scan_tf.py

--- scan_tf.py
+++ scan_tf.py
@@ -1,6 +1,5 @@
 import openai
 import os
-import fnmatch
 
 # Set the API key
 openai.api_key = os.environ["OPENAI_API_KEY"]
@@ -8,23 +7,20 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 # Set the prompt
 prompt = "Scan the Terraform files and code in the GitHub repository for AWS Foundations best practices."
 
-# Set the root directory
-root_dir = "."
+# Set the file path
+file_path = "."
 
-# Set the file pattern to search for
-file_pattern = "*.tf"
+# Read the files
+files = []
+for subdir, dirs, files in os.walk(file_path):
+    for file in files:
+        if file.endswith(".tf"):
+            file_path = os.path.join(subdir, file)
+            with open(file_path, "r") as f:
+                files.append(f.read())
 
-# Find all files matching the pattern in the root directory and its subdirectories
-tf_files = []
-for root, dirnames, filenames in os.walk(root_dir):
-    for filename in fnmatch.filter(filenames, file_pattern):
-        tf_files.append(os.path.join(root, filename))
-
-# Read the files and concatenate the code
-code = ''
-for tf_file in tf_files:
-    with open(tf_file) as f:
-        code += f.read()
+# Concatenate the files
+code = '\n'.join(files)
 
 # Call the GPT-3 API
 response = openai.Completion.create(
@@ -35,7 +31,7 @@ response = openai.Completion.create(
     temperature=0.7,
     stop=None,
     timeout=60,
-    text=code
+    data=code
 )
 
 # Get the response
