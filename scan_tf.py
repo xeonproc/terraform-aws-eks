59fcf6b78ba5fae172a3b91f5892cb406136dc9f
xeonproc <xeonproc@gmail.com>
2023-03-09 08:03:55 -0600

    Update scan_tf.py

--- scan_tf.py
+++ scan_tf.py
@@ -1,45 +1,35 @@
+# Set up OpenAI API key
 import openai
-import os
-import re
+from github import Github
 
-# Fetch the API key from the environment variable
+# Set up the OpenAI API
 openai.api_key = os.environ["OPENAI_API_KEY"]
+github_pat = os.environ["GH_PAT"]
 
-# Use the OpenAI API as before
-def generate_text(prompt):
-    completions = openai.Completion.create(
-        engine="davinci",
+# Get the repository you want to analyze
+repo = g.get_repo("xeonproc/terraform-aws-eks")
+
+# Get a list of all the files in the repository
+files = [file.path for file in repo.get_contents("") if file.type == "file"]
+
+# Iterate through each file and analyze its contents using the OpenAI API
+for file in files:
+    # Get the contents of the file
+    contents = repo.get_contents(file).decoded_content
+    
+    # Analyze the contents using the OpenAI API
+    prompt = "What are the security issues with this code? What are the line numbers of the finding in the file? What are the remediation recommendations?"
+    response = openai.Completion.create(
+        engine="text-davinci-002",
         prompt=prompt,
-        max_tokens=40,
+        max_tokens=1024,
         n=1,
         stop=None,
         temperature=0.5,
+        timeout=10,
     )
-    message = completions.choices[0].text.strip()
-    return message
-
-# Specify the path to the main.tf file in the root directory
-main_tf_path = "main.tf"
-
-# Check if the main.tf file exists in the root directory
-if not os.path.isfile(main_tf_path):
-    print(f"{main_tf_path} does not exist in the root directory")
-    exit(1)
-
-# Open the main.tf file and split its contents into smaller chunks
-with open(main_tf_path, "r") as f:
-    text = f.read()
-    prompt_chunks = re.findall(r'.{1,500}', text)
-
-# Generate completions for each chunk and join them into a single summary
-completions = []
-for chunk in prompt_chunks:
-    completions.append(generate_text(chunk))
-summary = ". ".join(completions)
-
-# Prompt OpenAI to scan the main.tf file for AWS foundational best practices
-prompt = "Please scan the following Terraform file for AWS foundational best practices:\n\n" + main_tf_path
-message = generate_text(prompt)
-
-# Print the result of the scan to the console
-print(message)
+    
+    # Print the results
+    print(f"File: {file}")
+    print(f"Prompt: {prompt}")
+    print(f"Response: {response.choices[0].text}")
