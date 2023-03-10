5519c1947f3ec4f7087a4ec08da7d5f3b1ee0369
xeonproc <xeonproc@gmail.com>
2023-03-10 08:48:12 -0600

    Update scan_tf.py

--- scan_tf.py
+++ scan_tf.py
@@ -14,24 +14,33 @@ repo = g.get_repo("xeonproc/terraform-aws-eks")
 # Get a list of all the files in the repository
 files = [file.path for file in repo.get_contents("") if file.type == "file"]
 
-# Iterate through each file and analyze its contents using the OpenAI API
+# Define the list of models to be used
+models = [
+    {"engine": "text-davinci-002", "name": "Deep Learning Model"},
+    {"engine": "text-babbage-001", "name": "Unsupervised Learning Model"},
+    {"engine": "text-curie-001", "name": "Supervised Learning Model"}
+]
+
+# Iterate through each file and analyze its contents using the OpenAI API with each model
 for file in files:
     # Get the contents of the file
     contents = repo.get_contents(file).decoded_content
     
-    # Analyze the contents using the OpenAI API
-    prompt = prompt =  f"What is the summary of the contents of {file}? Do you see terraform code in this file? If so what is this file doing and what security issues, risks or vulnerabilities do you see in the code? Include the code snippets that are vulnerable and use aws foundational best practices to guide your search of the terraform code."
-    response = openai.Completion.create(
-        engine="text-babbage-001",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
-        timeout=10,
-    )
-    
-    # Print the results
-    print(f"File: {file}")
-    print(f"Prompt: {prompt}")
-    print(f"Response: {response.choices[0].text}")
+    for model in models:
+        # Analyze the contents using the OpenAI API
+        prompt = f"What compliance issues and vulnerabilities do you see in {file}?"
+        response = openai.Completion.create(
+            engine=model["engine"],
+            prompt=prompt,
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.5,
+            timeout=10,
+        )
+        
+        # Print the results
+        print(f"File: {file}")
+        print(f"Model: {model['name']}")
+        print(f"Prompt: {prompt}")
+        print(f"Response: {response.choices[0].text}")
